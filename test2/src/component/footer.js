b4db1a58c8dd47ca17c1a9b0040e30623db68214
zahid-mughal <zahisohna@gmail.com>
2021-06-24 13:43:07 +0500

    import css file and give class as className

---
+++ test2/src/component/footer.js
@@ -0,0 +1,12 @@
+import './footer.css'
+function Footer(){
+return(
+    <div className="footerColor">
+        <h2>
+            This is footer
+        </h2>
+    </div>
+)
+
+}
+export default Footer;
