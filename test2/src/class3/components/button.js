129f7087aa3a36d97fd487be53275431ce4c5158
zahid-mughal <zahisohna@gmail.com>
2021-06-24 20:22:51 +0500

    add one more props

--- test2/src/class3/components/button.js
+++ test2/src/class3/components/button.js
@@ -1,7 +1,7 @@
 function Button(props){
     return(
         <div>
-            <button>{props.title}</button>
+            <button style={{color:props.color}}>{props.title}</button>
         </div>
     )
 }
