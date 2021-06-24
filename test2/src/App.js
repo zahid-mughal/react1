6ca44a58c8e1637874f42349eeb6061c6d1182a1
zahid-mughal <zahisohna@gmail.com>
2021-06-24 13:16:29 +0500

    import hello from header

---
+++ test2/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Hello from './component/header'
+
+function App() {
+  return (
+    <div className="App">
+      <Hello />
+      
+    </div>
+  );
+}
+
+export default App;
